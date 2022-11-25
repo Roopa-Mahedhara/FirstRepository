dacc63981c941fa6af987a1057e6f7544d919a2f
123 <123@DESKTOP-1H7BV0S>
2022-11-25 15:10:23 +0530

    Second Push

---
+++ src/helloWorld/Addition.java
@@ -0,0 +1,17 @@
+package helloWorld;
+
+public class Addition {
+
+	public void addition() {
+		int x=10;
+		int y=20;
+		System.out.println(x+y);
+	}
+	
+	public static void main(String[] args) {
+		Addition ad = new Addition();
+		ad.addition();
+
+	}
+
+}
